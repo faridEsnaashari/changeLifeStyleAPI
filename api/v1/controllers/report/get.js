7709ee395b790c2220c67bd3e96d2862df3259da
faridEsnaashari <faride13578642@gmail.com>
2023-03-27 16:05:00 +0330

    messages are edited if new message is for today

--- api/v1/controllers/report/get.js
+++ api/v1/controllers/report/get.js
@@ -5,22 +5,32 @@ const createTemplate = require("../../../templates");
 const { INTERNAL_ERR, SUCCESS_MSG } = require(`${ global.paths.tools.statusCodes }`);
 
 const get = async(req, res) => {
-    const data = req.query;
+    const { id, messageId: message_id, messageTime, ...data  } = req.query;
 
-    const date = new Date().toLocaleDateString('fa-IR-u-nu-latn', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    const persianDate = new Date().toLocaleDateString('fa-IR-u-nu-latn', { year: 'numeric', month: '2-digit', day: '2-digit' });
 
     const preparedData = {};
 
     Object.entries(data).forEach(([ key, value ]) => value === "true" ? preparedData[key] = true : preparedData[key] = false);
 
-    const text = createTemplate("report", { ...preparedData, date, id: data.id });
+    const text = createTemplate("report", { ...preparedData, date: persianDate, id });
+
+    const date = new Date().toISOString().split("T")[0];
+    const dawn = new Date(date);
+    const dawnTime = dawn.getTime();
 
     try{
-        await axios.post(`https://api.telegram.org/bot${ global.env.TELEGRAM.BOT_TOKEN }/sendMessage`, {
+        
+        const method = messageTime && +messageTime >= dawnTime ? "editMessageText" : "sendMessage";
+        const telegramRes = await axios.post(`https://api.telegram.org/bot${ global.env.TELEGRAM.BOT_TOKEN }/${ method }`, {
+            message_id,
             chat_id: global.env.TELEGRAM.CHAT_ID,
             text 
         });
-        res.responser(SUCCESS_MSG, "message has been sent to the channel");
+
+        res.responser(SUCCESS_MSG, "message has been sent to the channel", {
+            messageId: telegramRes.data.result.message_id,
+        });
     }
     catch(err){
         console.error(err);
